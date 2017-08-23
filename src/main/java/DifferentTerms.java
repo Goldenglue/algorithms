df350f810f0c33e41caa3caf4e1176340ae051fa
Goldenglue <ember.traveller@gmail.com>
2017-08-24 02:32:50 +0700

    different terms of number algorithm

---
+++ src/main/java/DifferentTerms.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DifferentTerms {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        generateTerms(1);
+    }
+
+    private static void generateTerms(int n) {
+        List<Integer> ks = new ArrayList<>();
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n == 1) {
+                ks.add(n);
+                break;
+            }
+            if (total + i*2 + 1 > n) {
+                ks.add(n - total);
+                break;
+            }
+            ks.add(i);
+            total += i;
+        }
+        System.out.println(ks.size());
+        ks.forEach(System.out::println);
+    }
+}
