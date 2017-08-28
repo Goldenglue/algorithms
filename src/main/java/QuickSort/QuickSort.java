770476c37845a5bd83850396ddda9add1a511411
Goldenglue <ember.traveller@gmail.com>
2017-08-29 02:01:34 +0700

    eliminated second recursion

--- src/main/java/QuickSort/QuickSort.java
+++ src/main/java/QuickSort/QuickSort.java
@@ -10,30 +10,35 @@ public class QuickSort {
     }
 
     private void quickSort(int[] array, int l, int r) {
-        if (l < r) {
+        while (l < r) {
             int k = l + random.nextInt(r - l);
+
             int temp  = array[l];
             array[l] = array[k];
             array[k] = temp;
+
             int m = partition(array, l, r);
+
             quickSort(array, l, m - 1);
-            quickSort(array, m + 1, r);
+            l = m + 1;
         }
     }
 
     private int partition(int[] arr, int low, int high) {
         int x = arr[low];
         int i = low;
+
         for (int j = low + 1; j <= high; j++) {
             if (arr[j] <= x) {
                 i++;
                 swap(arr,i,j);
             }
         }
+
         swap(arr,i,low);
+
         return i;
     }
-
 
     private void swap(int[] array, int l, int r) {
         int temp = array[l];
